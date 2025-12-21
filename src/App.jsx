c994ba8724db0c0d69ab2b393d6aa30947c97e2c
Abdulatif <saddiabu4@gmail.com>
2025-12-21 23:48:53 +0500

    refactor: remove unused components and hooks, update main entry point, and clean up admin pages
    - Deleted `use-mobile.js` hook as it was no longer needed.
    - Removed `Dashboard`, `Inbox`, `NotFound`, `Products`, `AdminCarts`, `AdminProducts`, and `AdminUsers` components to streamline the codebase.
    - Updated `main.jsx` to reflect changes in import paths and formatting.
    - Cleaned up the router by removing routes associated with deleted components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { CartProvider } from "@/context/CartContext"
-import Router from "@/router/Router"
-import { BrowserRouter } from "react-router-dom"
-import { Toaster } from "sonner"
+import React from "react"
+import { Button } from "@/components/ui/button"
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<CartProvider>
-				<Router />
-				<Toaster richColors position='top-right' />
-			</CartProvider>
-		</BrowserRouter>
+		<div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+			<h1 className='text-3xl font-bold underline text-center'>Hello world!</h1>
+			<Button variant='outline' type='button'>
+				Click me
+			</Button>
+		</div>
 	)
 }
 
